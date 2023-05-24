eb61b2762dd7ba14210bb85b1f2d8da21b4060a2
Q-holi <32233160+Q-holi@users.noreply.github.com>
2023-05-24 22:46:11 +0900

    Error Logging and String Conversion

--- DirectX 11 Engine/Source.cpp
+++ DirectX 11 Engine/Source.cpp
@@ -1,13 +1,16 @@
-#include <Windows.h>
+#include "ErrorLogger.h"
 #pragma comment(lib,"d3d11.lib")
 #pragma comment(lib,"DirectXTK.lib")
 
-
-//
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	_In_opt_ HINSTANCE hPrevInstance,
 	_In_ LPWSTR    lpCmdLine,
 	_In_ int       nCmdShow)
 {
+	HRESULT hr = E_INVALIDARG;
+	if (FAILED(hr)) 
+	{
+		ErrorLogger::Log(hr, "FAILURE");
+	}
 	return 0;
 }
